39030d8718c604af3239d43eb6e60579d9aab95e
Ivan López Rodríguez <ivaan100@gmail.com>
2025-09-20 18:23:35 +0200

    add: ✨ dynamic title name - observable & signal

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,8 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { NavigationEnd, Router, RouterLink } from '@angular/router';
+import { filter, map } from 'rxjs';
+import { toSignal } from '@angular/core/rxjs-interop'
+import { AsyncPipe } from '@angular/common';
+
+import { routes } from '../../../app.routes';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [AsyncPipe, RouterLink],
   templateUrl: './navbar.component.html',
 })
-export class NavbarComponent { }
+export class NavbarComponent {
+  router = inject(Router);
+
+  routes = routes.map((route) => ({
+    path: route.path,
+    title: `${route.title ?? 'Maps en Angular'}`,
+  })).filter((route) => route.path !== '**');
+
+  // Observable way
+  pageTitle$ = this.router.events.pipe(
+    filter((event) => event instanceof NavigationEnd),
+    // tap((event) => console.log(event)),
+    map((event) => event.url),
+    map((url) => routes.find((route) => `/${route.path}` === url)?.title ?? 'Mapas'
+    )
+  );
+
+  // Signal way
+  // pageTitle = toSignal(this.router.events.pipe(
+  //   filter((event) => event instanceof NavigationEnd),
+  //   // tap((event) => console.log(event)),
+  //   map((event) => event.url),
+  //   map((url) => routes.find((route) => `/${route.path}` === url)?.title ?? 'Mapas'
+  //   )
+  // ))
+}
